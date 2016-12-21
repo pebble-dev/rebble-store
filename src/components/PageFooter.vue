106b0307062410c49138e344ce419952e2e5c8b3
Gary Daigle <zetaphor@gmail.com>
2016-12-21 17:42:20 -0500

    Added style tag to footer component

--- src/components/PageFooter.vue
+++ src/components/PageFooter.vue
@@ -1,7 +1,7 @@
 <template>
   <footer>
-     <p>© 2016 Rebble · <a href="#">Contact Us</a> · <a href="#">Terms</a></p>
-     <a class="pull-right" href="#">Developer Portal</a>
+   <p>© 2016 Rebble · <a href="#">Contact Us</a> · <a href="#">Terms</a></p>
+   <a class="pull-right" href="#">Developer Portal</a>
   </footer>
 </template>
 
@@ -10,3 +10,6 @@ export default {
   name: 'page-footer'
 }
 </script>
+
+<style lang="scss" scoped>
+</style>
