1eff5c682216ed902c370b1b520e5eb8471be50e
Gary Daigle <zetaphor@gmail.com>
2016-12-21 17:30:58 -0500

    Moved base app into Home component
    This will make things easier to follow when we start using other stuff like Vuex or vue-router

---
+++ src/components/Home.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <home-slider></home-slider>
+      <main class="apps container text-xs-center">
+        <fresh-picks></fresh-picks>
+        <fresh-picks></fresh-picks>
+      </main>
+  </div>
+</template>
+
+<script>
+import FreshPicks from './FreshPicks'
+import HomeSlider from './HomeSlider'
+
+import RouterHome from './router/RouterHome'
+import RouterAppDetails from './router/RouterAppDetails'
+import RouterSearch from './router/RouterSearch'
+import RouterAppVersion from './router/RouterAppVersion'
+import RouterApps from './router/RouterApps'
+import RouterError from './router/RouterError'
+import RouterAuthor from './router/RouterAuthor'
+import RouterFeatured from './router/RouterFeatured'
+import RouterCategory from './router/RouterCategory'
+
+export default {
+  name: 'Home',
+  components: {
+    FreshPicks,
+    HomeSlider,
+    RouterAppDetails,
+    RouterHome,
+    RouterSearch,
+    RouterAppVersion,
+    RouterApps,
+    RouterError,
+    RouterAuthor,
+    RouterFeatured,
+    RouterCategory
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
