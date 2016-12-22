90299a1ded9f1c1d8b7e98f8ecee4166e2c551d3
Gary Daigle <zetaphor@gmail.com>
2016-12-22 15:33:05 -0500

    Restored search and watchfaces icon. pet-rock and apps icon need work

--- src/components/SvgContainer.vue
+++ src/components/SvgContainer.vue
@@ -45,7 +45,7 @@
       </g>
     </svg>
 
-    <svg id="svgPetRock" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+    <svg id="petRock" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
       <defs>
         <polygon id="a" points="160.865 162.763 243.499 144.619 257.327 109.929 242.053 72.586 209.66 56.935 180.398 34.881 138.125 29.331 89.961 41.943 58.829 43.073 31.846 60.054 13.107 99.878 36.862 138.39 100.393 158.597"/>
         <mask id="d" width="244.22" height="133.432" x="0" y="0" fill="white">
@@ -77,6 +77,45 @@
         </text>
       </g>
     </svg>
+
+    <svg id="iconSearch" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+      <g id="Icons" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+        <g id="Search" stroke="#a5a6a7">
+          <path id="Shape" stroke-width="1.5" class="st0" fill="none" d="M13.9,16.6l1.2-0.4c0,0,0.8-1.5,0.8-1.6c2.3,2.4,4.7,4.7,7,7.1l-2,2
+          C18.5,21.2,16.2,18.9,13.9,16.6z"/>
+          <polygon id="Shape_1_" stroke-width="2" fill="none" class="st1" points="2.2,10.2 4.6,5 10.3,3 15.4,5.4 17.5,11 15.1,16.1 9.4,18.2 4.3,15.8 		"/>
+          <path id="Line" fill="none" class="st2" d="M11.3,6.6l2,1l1,3" stroke-linecap="square"/>
+        </g>
+      </g>
+    </svg>
+
+    <svg id="iconApp" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+      <defs>
+        <polygon id="path-1" points="9 4 19 4 21 6 21 16 19 18 9 18 7 15.9960225 7 6"></polygon>
+        <mask id="mask-2" maskContentUnits="userSpaceOnUse" maskUnits="objectBoundingBox" x="-2" y="-2" width="18" height="18">
+          <rect x="5" y="2" width="18" height="18" fill="white"></rect>
+          <use xlink:href="#path-1" fill="black"></use>
+        </mask>
+      </defs>
+      <g id="Icons" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+        <g id="App">
+          <g id="Rectangle-2">
+            <use fill="none" fill-rule="evenodd" xlink:href="#path-1"></use>
+            <use id="use2" stroke="rgba(204,204,204,.5)" mask="url(#mask-2)" stroke-width="4" xlink:href="#path-1"></use>
+          </g>
+          <polyline id="Line" stroke="rgba(204,204,204,.5)" stroke-width="2" stroke-linecap="square" points="17 22.0005626 7 22.0005626 3 18 3 8"></polyline>
+        </g>
+      </g>
+    </svg>
+
+    <svg id="iconWatchface" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+      <g id="Icons" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+        <g id="Clock" stroke-width="2" stroke="rgba(204,204,204,.5)">
+          <polygon id="Shape" fill="none" points="2 8.00285097 8 2 17 2 23 8.00285097 23 17.0255772 17 23 8 23 2 17.0255772"></polygon>
+          <polyline id="Line" stroke-linecap="square" points="15.5630383 14.7579965 12.040745 12.1503281 12 6"></polyline>
+        </g>
+      </g>
+    </svg>
   </div>
 </template>
 
@@ -96,7 +135,20 @@ export default {
     fill: inherit;
   }
 
-  .icon-thumbs-up {
+  .btn-watchface svg, .btn-app svg {
+    fill: #363a3d;
+    stroke: #828682;
+  }
+
+  .btn-watchface.active svg,
+  .btn-app.active svg,
+  .btn-watchface:hover svg,
+  .btn-app:hover svg {
+    fill: #858585;
+    stroke: #FFF;
+  }
+
+  .icon-search, .icon-thumbs-up {
     fill: #333;
     stroke: #ccc;
   }
@@ -117,6 +169,11 @@ export default {
   .btn-download:hover svg {
     fill: #ff4700;
     stroke: #333;
+  }
+
+  .pet-rock-pebble {
+    fill: initial;
+    stroke: initial;
   }
 
 
