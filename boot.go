563191640acca2f9ca9b6074189bee6fe8f8960c
Henry López del Pino <h.lopez.0907@institutonacional.cl>
2016-12-22 01:07:53 -0300

    Fixed custom store_uri

--- boot.go
+++ boot.go
@@ -63,16 +63,19 @@ func BootHandler(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Invalid store_uri parameter"))
 		return
 	}
-
+	
+	// Copying the URL Query to modify it later
+	urlquery := r.URL.Query()
+	
 	// If the user didn't specify a store_uri, use the pebble server
 	if store_uri == "" {
 		store_uri = STORE_URI
 	} else {
-		r.URL.Query().Del("store_uri")
+		urlquery.Del("store_uri")
 	}
-
+	
 	// Build up the request URL
-	request_url := fmt.Sprintf("%s%s?%s", PEBBLE_BOOT_URL, mux.Vars(r)["path"], r.URL.RawQuery)
+	request_url := fmt.Sprintf("%s%s?%s", PEBBLE_BOOT_URL, mux.Vars(r)["path"], urlquery.Encode())
 
 	// Make a request to an external server then parse the request
 	req, err := http.Get(request_url)
