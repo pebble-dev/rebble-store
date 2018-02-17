61e1d54915bb9ffa9f50551902189c237528360c
MHC03 <murathancelik@gmx.de>
2018-02-17 21:17:06 +0100

    Optimize dragscroll
    Removed unnecessary test construction and matched dragscroll to live demo dragscroll.

--- src/components/pages/widgets/ScreenshotList.vue
+++ src/components/pages/widgets/ScreenshotList.vue
@@ -1,11 +1,9 @@
 <template>
   <div v-dragscroll.x="true" class="screenshots">
-      <div id="scrollbar">
+      <div id="imageDiv">
           <single-screenshot v-for="(screenshot, index) in getPlatform().screenshots" v-bind:key="index" v-bind:screenshotSrc="screenshot"></single-screenshot>
-          <!-- Screenshots -->
-          <div style="width: 184px;height: 100px; background-color: #006600;  margin-right: 40px;display: table;position: absolute"></div>
-          <div class="screenshot-spacer" style="background-color: cadetblue"></div>
       </div>
+      <div id="scrollbar" v-bind:style="scrollStyle"> </div>
   </div>
 </template>
 
@@ -21,23 +19,35 @@ export default {
   components: {
     SingleScreenshot
   },
+  data: function () {
+    return {
+      screenshotsCount: 0
+    }
+  },
   props: ['platforms', 'clientWatchPlatform'],
   methods: {
     getPlatform: function () {
       var that = this
       for (var i = 0; i < this.platforms.length; ++i) {
         if (this.platforms[i].platform === that.clientWatchPlatform && this.platforms[i].screenshots.length > 0) {
+          this.screenshotsCount = this.platforms[i].screenshots.length
           return this.platforms[i]
         }
       }
 
       for (i = 0; i < this.platforms.length; ++i) {
         if (this.platforms[i].screenshots.length > 0) {
+          this.screenshotsCount = this.platforms[i].screenshots.length
           return this.platforms[i]
         }
       }
-
+      this.screenshotsCount = 0
       return []
+    }
+  },
+  computed: {
+    scrollStyle: function () {
+      return {'padding-right': 'calc(50% - 72px + ' + this.screenshotsCount + ' * 144px)'}
     }
   }
 }
@@ -50,7 +60,6 @@ export default {
     overflow-x: scroll;
     overflow-y: hidden;
     -webkit-overflow-scrolling: touch;
-    display: flex;
     margin-bottom: 40px;
     -ms-overflow-style: none;
     overflow: -moz-scrollbars-none;
@@ -59,19 +68,16 @@ export default {
         background: transparent;
         width: 0 !important;
     }
-    // This spacer is the one that centers the first screenshot
-    .screenshot-spacer {
-        width: calc(50vw - 92px);
-        height: 168px;
-        display: table;
+}
 
-    }
+#imageDiv {
+    // -72px for centering first screenshot
+    padding-left: calc(50% - 72px);
 }
 
 #scrollbar {
     display: inline-block;
-    padding-left: 50vw;
-    padding-right: 50vw;
+    padding-left: calc(50% - 72px);
     background-color: #9b9d9e;
 }
 
