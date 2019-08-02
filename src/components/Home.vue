ec605e1b988dcf2eaaf282acfb986ded2c53809a
Stefano Gerli <sgerli@igerli.com>
2019-08-02 09:44:12 -0600

    Fix empty app columns on home

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
     <slider v-bind:banners="page.banners"></slider>
-    <main class="apps container">
+    <main class="home apps container">
       <tag-list v-if="type != 'faces'" v-bind:tags="page.categories"></tag-list>
-      <card-collection v-for="(collection, index) in page.collections"  v-bind:key="index" v-bind:elTitle="collection.name" v-bind:cards="collection.data" v-bind:allUrl="'/' + type + '/' + collection.slug"></card-collection>
+      <card-collection v-for="(collection, index) in page.collections"  v-bind:key="index" v-bind:elTitle="collection.name" v-bind:cards="collection.data" v-bind:allUrl="`/${type}/${collection.slug}`"></card-collection>
     </main>
   </div>
 </template>
@@ -12,6 +12,8 @@
 import CardCollection from './pages/widgets/CardCollection'
 import TagList from './pages/widgets/TagList'
 import Slider from './pages/widgets/HomeSlider'
+
+const MAX_HOME_COLLECTION_SIZE = 6
 
 // mostRecentCards and freshPicksCards are placeholders. Todo: actually fetch those from the server.
 export default {
@@ -47,6 +49,10 @@ export default {
       for (let collection of this.page.collections) {
         collection.data = []
         for (let id of collection.application_ids) {
+          if (collection.data.length >= MAX_HOME_COLLECTION_SIZE) {
+            // Limit collection size
+            break
+          }
           collection.data.push(this.page.applications.find(function (app) {
             return id === app.id
           }))
@@ -60,5 +66,13 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
+main.home {
+  @media screen and (min-width: 992px) {
+    section .card-columns {
+        column-count: 3 !important;
+    }
+  }
+}
+
 </style>
